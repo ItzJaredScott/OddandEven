98c0d9c5c23d2c9faea3b6c9ad156b99603d4f0d
ImHawkeye <167693724+ImHawkeye@users.noreply.github.com>
2025-09-18 09:17:34 -0500

    added files for the program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.WriteLine("Give me a number!");
+    int x = Convert.ToInt32(Console.ReadLine()); //converts the number given to an int
+
+if (x % 2 == 0) //finds if there is a remainder using 2
+{
+    Console.WriteLine($"{x} is even."); 
+}
+else
+{
+    Console.WriteLine($"{x} is odd.");
+}
+
+Console.ReadLine();
